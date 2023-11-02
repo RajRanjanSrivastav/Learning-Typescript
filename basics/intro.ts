edf00ac72922b3635c1d7121ae10ac916f9df5ea
RAJ RANJAN SRIVASTAV <srivastavrajranjan75@gmail.com>
2023-11-02 15:03:28 +0530

    more about types

--- basics/intro.ts
+++ basics/intro.ts
@@ -8,6 +8,15 @@ console.log(naam);
 let uppercase = naam.toUpperCase();
 console.log(uppercase);
 
+
+let name = "raj ranjan srivastav";  // this is a good way to write it without collon : beacause tyescript is smarter enough it automaticaly detech what is inside......so you don't worry about it
+
+typeof(name)
+
+// name=4;  //we cant do this in typescript
+
+export default name;
+
 //please note down tsc is the compiler of typescript which is used to compile it .... and at the end what execute at browser is pure javascript......
 
 
