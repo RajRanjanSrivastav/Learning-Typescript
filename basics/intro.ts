cd8d121ff3df5694c0c50a5d7da4ed5d4b867397
RAJ RANJAN SRIVASTAV <srivastavrajranjan75@gmail.com>
2023-11-02 14:52:14 +0530

    first commit learnig about typescript

---
+++ basics/intro.ts
@@ -0,0 +1,14 @@
+// so in this file we are going to learn about types in typescript
+
+// how you declare a variable in typescript
+
+let naam : string = "raj ranjan srivastav";
+console.log(naam);
+ 
+let uppercase = naam.toUpperCase();
+console.log(uppercase);
+
+//please note down tsc is the compiler of typescript which is used to compile it .... and at the end what execute at browser is pure javascript......
+
+
+// in typescript we get errors if we write something wrong syntax but in javascript..... there is nothing ... and it might be run 
